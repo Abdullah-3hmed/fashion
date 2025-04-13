aca82422399f1aaf171f1018062caa040617ce07
Abdullah <abdullhahmed107@gmail.com>
2025-04-13 16:45:17 +0200

    modify order status

--- lib/features/profile/screens/widgets/order_item.dart
+++ lib/features/profile/screens/widgets/order_item.dart
@@ -26,36 +26,63 @@ class OrderItem extends StatelessWidget {
             borderRadius: BorderRadius.circular(16.0),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Image.asset(
-              width: 90.0,
-              height: 100.0,
+              width: 94.0,
+              height: 94.0,
               fit: BoxFit.cover,
-              AssetsManager.welcomeImage,
+              AssetsManager.onProgressImage,
             ),
           ),
-          const SizedBox(width: 24.0),
+          const SizedBox(width: 16.0),
           Column(
-            mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "Black jacket",
-                style: Theme.of(context).textTheme.titleMedium,
+              Row(
+                children: [
+                  Text(
+                    "Order Id",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  const SizedBox(width: 10.0),
+                  Text(
+                    "#25654568",
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.zero,
+                    icon: Icon(
+                      Icons.copy,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 4.0),
-              Text("Size : XL", style: Theme.of(context).textTheme.bodySmall),
+              Row(
+                children: [
+                  Text(
+                    r"$ Price",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  const SizedBox(width: 14.0),
+                  Text(
+                    r"$600.00",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
               const SizedBox(height: 16.0),
-              Text(r"$200.00", style: Theme.of(context).textTheme.bodySmall),
-            ],
-          ),
-          const Spacer(),
-          Align(
-            alignment: AlignmentDirectional.bottomEnd,
-            child: Text(
-              "On progress",
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                color: Theme.of(context).colorScheme.primary,
+              SizedBox(
+                width: 178.0,
+                child: Text(
+                  "On Progress",
+                  textAlign: TextAlign.end,
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
