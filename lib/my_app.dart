005a0bbefc58efb907b52388bda424e2af437bf3
Abdullah <abdullhahmed107@gmail.com>
2025-05-24 14:09:09 +0300

    finish search products

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeManager.darkTheme(),
+      theme: ThemeManager.lightTheme(),
       routerConfig: getIt<AppRouter>().config(
         navigatorObservers: () => [AppRoutesObserver(), AutoRouteObserver()],
       ),
